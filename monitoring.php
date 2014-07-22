752d87ab65be26e8ee93abe44ae664a707a666f3
FXHibon <hibonfx@gmail.com>
2014-07-23 00:27:27 +0200

    update should works

--- monitoring.php
+++ monitoring.php
@@ -5,11 +5,51 @@
  
 header('Content-type: application/json');
 
+/**
+* @link http://gist.github.com/385876
+*/
+function csv_to_array($filename='', $delimiter=',')
+{
+    if(!file_exists($filename) || !is_readable($filename))
+        return FALSE;
+
+    $header = NULL;
+    $data = array();
+    if (($handle = fopen($filename, 'r')) !== FALSE)
+    {
+        while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+        {
+            if(!$header)
+                $header = $row;
+            else
+                $data[] = array_combine($header, $row);
+        }
+        fclose($handle);
+    }
+    return $data;
+}
+
+$imageNames = array("node.exe", "mongod.exe", "httpd.exe");
+
 function update() {
-	$res = array(
-	    'date'      => date('d/m/Y H:i:s'),
-	    'phpversion'=> phpversion()
-	);
+	exec('tasklist /FI "imagename eq node.exe" /fo csv > results.txt');
+	exec('tasklist /FI "imagename eq mongod.exe" /fo csv /nh >> results.txt');
+	exec('tasklist /FI "imagename eq httpd.exe" /fo csv /nh >> results.txt');
+
+	$csv = csv_to_array("results.txt");
+
+	$res = array();
+	foreach ($csv as $key => $value) {
+		$res[$value["Nom de l'image"]] = array();
+		$res[$value["Nom de l'image"]]["status"] = "success";
+	}
+
+	foreach ($imageNames as $key => $value) {
+		if (!in_array($key, $results)) {
+			$results[$key] = array("status" => "danger")
+		}
+	}
+
 	return $res;
 }
 
