bf24c44d8350468dde197e7902b79ea1c3095607
FXHibon <djxf44@gmail.com>
2014-07-29 23:23:03 +0200

    Implémentation du service web correspondant à la liste des serveurs actifs. Suppression du js inutile. Rename des images. Amélioration du css

--- monitoring.php
+++ monitoring.php
@@ -13,16 +13,12 @@ function csv_to_array($filename='', $delimiter=',')
     if(!file_exists($filename) || !is_readable($filename))
         return FALSE;
 
-    $header = NULL;
     $data = array();
     if (($handle = fopen($filename, 'r')) !== FALSE)
     {
         while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
         {
-            if(!$header)
-                $header = $row;
-            else
-                $data[] = array_combine($header, $row);
+            $data[] = $row;
         }
         fclose($handle);
     }
@@ -33,25 +29,37 @@ function csv_to_array($filename='', $delimiter=',')
 
 function update() {
 	$imageNames = array("node.exe", "mongod.exe", "httpd.exe");
-	exec('tasklist /FI "imagename eq node.exe" /fo csv > results.txt');
+	exec('tasklist /FI "imagename eq node.exe" /fo csv /nh > results.txt');
 	exec('tasklist /FI "imagename eq mongod.exe" /fo csv /nh >> results.txt');
 	exec('tasklist /FI "imagename eq httpd.exe" /fo csv /nh >> results.txt');
 
 	$csv = csv_to_array("results.txt");
+	//var_dump($csv);
+	$res = "";
 
-	$res = array();
-	foreach ($csv as $key => $value) {
-		$res[$value["Nom de l'image"]] = array();
-		$res[$value["Nom de l'image"]]["status"] = "success";
+	$processedProcess = [];
+
+	// traitements des processus actifs
+	foreach ($csv as $value) {
+		if (in_array($value[0], $imageNames) ) {
+			$processedProcess[] = $value[0];
+			$tmp = '{"name": "' . $value[0] . '", "state": "running", "icon": "/dist/images/' . $value[0] . '-icon.png"}';
+			$res .= $tmp . ", ";
+		}
 	}
 
-/*
-	foreach ($imageNames as $key => $value) {
-		if (!in_array($value, $res)) {
-			$res[$key] = array("status" => "danger");
+	// traitements des processus inactifs
+	foreach ($imageNames as $imageName) {
+		if (!in_array($imageName, $processedProcess)) {
+			$tmp = '{"name": "' . $imageName . '", "state": "down", "icon": "/dist/images/' . $imageName . '-icon.png"}';
+			$res .= $tmp . ", ";
 		}
-	}*/
+	}
+	$res = substr($res, 0, -2);
 
+	echo "[" . $res . "]";
+
+	exit();
 	return $res;
 }
 
